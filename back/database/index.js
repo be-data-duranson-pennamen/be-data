31c9d6a8bdc285bc3116cefcd92618503a71b75d
unknown <thomas.pennamen@gmail.com>
2020-10-15 12:26:59 +0200

    added sql schema

--- back/database/index.js
+++ back/database/index.js
@@ -4,6 +4,6 @@ const airportDatabase = mysql.createPool({
   host: process.env.DATABASE_IP,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_AIRPORT,
+  database: process.env.DATABASE_NAME,
 })
 module.exports.airportDatabase = airportDatabase
