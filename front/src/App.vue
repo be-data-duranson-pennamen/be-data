baab4b9abf4a2211f4a38e394370ea66ff40b95d
unknown <thomas.pennamen@gmail.com>
2020-11-09 16:31:12 +0100

    Added initial front

---
+++ front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img class='background'/>
+    <navbar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/navbar'
+export default {
+  components: {Navbar}
+};
+</script>
+
+<style lang="css">
+@import "style/style.css";
+</style>
