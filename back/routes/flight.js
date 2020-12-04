35d6644409e89ff011d0f54c7f1544a67fd45f36
PolluxD <fabienpollux@gmail.com>
2020-12-04 19:00:49 +0100

    add flights

--- back/routes/flight.js
+++ back/routes/flight.js
@@ -10,11 +10,7 @@ module.exports = new Router()
             .then(found => res.json(found))
             .catch(next);
     })
-<<<<<<< HEAD
-    .post("/find", (req, res, next) => {
-=======
     .post("/", (req, res, next) => {
->>>>>>> d7044b8dce87b9688f2e821afb3174cfdccbcbd9
         flight
             .findOne(req)
             .then(found => res.json(found))
